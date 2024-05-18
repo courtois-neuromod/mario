4c662e87f53e4df4e9eed9ff55d9b21404ba955e
Yann Harel <yharel109@gmail.com>
2024-05-17 23:23:28 -0400

    added frames + scenes

--- code/annotations/generate_annotations.py
+++ code/annotations/generate_annotations.py
@@ -17,15 +17,21 @@ parser.add_argument(
     help="Data path to look for events.tsv and .bk2 files. Should be the root of the mario dataset.",
 )
 parser.add_argument(
-    "-s",
+    "-st",
     "--stimuli",
     default='./stimuli',
     type=str,
     help="Data path to look for the stimuli files (rom, state files, data.json etc...).",
 )
+parser.add_argument(
+    "-s",
+    "--scenesfile",
+    default=None,
+    type=str,
+    help="Path to the scenes file, a JSON file that contains info about the start and end positions to clip.",
+)
 
-
-def create_runevents(runvars, events_dataframe, FS=60):
+def create_runevents(runvars, scenes_info_dict, events_dataframe, FS=60):
     """Create a BIDS compatible events dataframe from game variables and start/duration info of repetitions
 
     Parameters
@@ -87,6 +93,11 @@ def create_runevents(runvars, events_dataframe, FS=60):
             temp_df['onset'] = temp_df['onset'] + repvars['rep_onset']
             all_df.append(temp_df)
 
+            # Scenes
+            temp_df = generate_scene_events(repvars, scenes_info_dict, FS=FS)
+            temp_df['onset'] = temp_df['onset'] + repvars['rep_onset']
+            all_df.append(temp_df)
+
     try:
         events_df = pd.concat(all_df).sort_values(by='onset').reset_index(drop=True)
     except ValueError:
@@ -112,6 +123,7 @@ def generate_key_events(repvars, key, FS=60):
         An events DataFrame in BIDS-compatible format containing the
         corresponding action events.
     """
+
     var = np.multiply(repvars[key], 1)
     # always keep the first and last value as 0 so diff will register the state transition
     var[0] = 0
@@ -121,6 +133,8 @@ def generate_key_events(repvars, key, FS=60):
     diffs = list(np.diff(var_bin, n=1))
     presses = [round(i/FS, 3) for i, x in enumerate(diffs) if x == 1]
     releases = [round(i/FS, 3) for i, x in enumerate(diffs) if x == -1]
+    frame_start = [i for i, x in enumerate(diffs) if x == 1]
+    frame_stop = [i for i, x in enumerate(diffs) if x == -1]
     onset = presses
     level = [repvars["level"] for x in onset]
     duration = [round(releases[i] - presses[i], 3) for i in range(len(presses))]
@@ -128,7 +142,9 @@ def generate_key_events(repvars, key, FS=60):
     events_df = pd.DataFrame(data={'onset':onset,
                                    'duration':duration,
                                    'trial_type':trial_type,
-                                   'level':level})
+                                   'level':level,
+                                   'frame_start':frame_start,
+                                   'frame_stop':frame_stop})
     return events_df
 
 def generate_kill_events(repvars, FS=60):
@@ -150,6 +166,8 @@ def generate_kill_events(repvars, FS=60):
     duration = []
     trial_type = []
     level = []
+    frame_start = []
+    frame_stop = []
 
     killvals_dict = {4:'stomp',
                      34:'impact',
@@ -168,16 +186,22 @@ def generate_kill_events(repvars, FS=60):
                         duration.append(0)
                         trial_type.append(killstring)
                         level.append(repvars['level'])
+                        frame_start.append(frame_idx)
+                        frame_stop.append(frame_idx)
                 else:
                     onset.append(frame_idx/FS)
                     duration.append(0)
                     trial_type.append(killstring)
                     level.append(repvars['level'])
+                    frame_start.append(frame_idx)
+                    frame_stop.append(frame_idx)
 
     events_df = pd.DataFrame(data={'onset':onset,
                                'duration':duration,
                                'trial_type':trial_type,
-                               'level':level})
+                               'level':level,
+                               'frame_start':frame_start,
+                               'frame_stop':frame_stop})
     return events_df
 
 def generate_hits_taken_events(repvars, FS=60):
@@ -185,6 +209,8 @@ def generate_hits_taken_events(repvars, FS=60):
     duration = []
     trial_type = []
     level = []
+    frame_start = []
+    frame_stop = []
 
     # Powerup lost
     diff_state = list(np.diff(repvars['powerstate']))
@@ -194,6 +220,8 @@ def generate_hits_taken_events(repvars, FS=60):
             duration.append(0)
             trial_type.append('Hit/powerup_lost')
             level.append(repvars['level'])
+            frame_start.append(idx_val)
+            frame_stop.append(idx_val)
 
     # Lives lost
     diff_lives = list(np.diff(repvars['lives']))
@@ -203,11 +231,15 @@ def generate_hits_taken_events(repvars, FS=60):
             duration.append(0)
             trial_type.append('Hit/life_lost')
             level.append(repvars['level'])
+            frame_start.append(idx_val)
+            frame_stop.append(idx_val)
 
     events_df = pd.DataFrame(data={'onset':onset,
                             'duration':duration,
                             'trial_type':trial_type,
-                            'level':level})
+                            'level':level,
+                            'frame_start':frame_start,
+                            'frame_stop':frame_stop})
     return events_df
 
 def generate_bricks_smashed_events(repvars, FS=60):
@@ -216,6 +248,8 @@ def generate_bricks_smashed_events(repvars, FS=60):
     duration = []
     trial_type = []
     level = []
+    frame_start = []
+    frame_stop = []
 
     score_increments = list(np.diff(repvars['score']))
     for idx_val, inc in enumerate(score_increments):
@@ -225,11 +259,15 @@ def generate_bricks_smashed_events(repvars, FS=60):
                 duration.append(0)
                 trial_type.append('Brick_smashed')
                 level.append(repvars['level'])
+                frame_start.append(idx_val)
+                frame_stop.append(idx_val)
 
     events_df = pd.DataFrame(data={'onset':onset,
                                'duration':duration,
                                'trial_type':trial_type,
-                               'level':level})
+                               'level':level,
+                               'frame_start':frame_start,
+                               'frame_stop':frame_stop})
     return events_df
 
 def generate_coin_events(repvars, FS=60):
@@ -237,7 +275,9 @@ def generate_coin_events(repvars, FS=60):
     duration = []
     trial_type = []
     level = []
-    
+    frame_start = []
+    frame_stop = []
+
     diff_coins = np.diff(repvars['coins'])
     for idx_val, val in enumerate(diff_coins):
         if val > 0:
@@ -245,11 +285,15 @@ def generate_coin_events(repvars, FS=60):
             duration.append(0)
             trial_type.append('Coin_collected')
             level.append(repvars['level'])
+            frame_start.append(idx_val)
+            frame_stop.append(idx_val)
 
     events_df = pd.DataFrame(data={'onset':onset,
                                'duration':duration,
                                'trial_type':trial_type,
-                               'level':level})
+                               'level':level,
+                               'frame_start':frame_start,
+                               'frame_stop':frame_stop})
     return events_df
 
 def generate_powerup_events(repvars, FS=60):
@@ -257,6 +301,8 @@ def generate_powerup_events(repvars, FS=60):
     duration = []
     trial_type = []
     level = []
+    frame_start = []
+    frame_stop = []
 
     ''' ### Currently broken
     diff_powerup = np.diff(repvars['powerup_yes_no'])
@@ -279,14 +325,75 @@ def generate_powerup_events(repvars, FS=60):
                 duration.append(0)
                 trial_type.append('Powerup_collected')
                 level.append(repvars['level'])
+                frame_start.append(idx)
+                frame_stop.append(idx)
     
     events_df = pd.DataFrame(data={'onset':onset,
                                'duration':duration,
                                'trial_type':trial_type,
-                               'level':level})
+                               'level':level,
+                               'frame_start':frame_start,
+                               'frame_stop':frame_stop})
+    return events_df
+
+def generate_scene_events(repvars, scenes_info_dict, FS=60):
+    onset = []
+    duration = []
+    trial_type = []
+    level = []
+    frame_start = []
+    frame_stop = []
+
+
+    curr_level = repvars['filename'].split("/")[-1].split("_")[-2].split('-')[1]
+    if curr_level == 'w1l1': ## remove/replace later
+        n_frames_total = len(repvars['score'])
+        # Create player_x_pos from Hi and Lo
+        repvars['player_x_pos'] = []
+        for idx in range(n_frames_total):
+            repvars['player_x_pos'].append(repvars['player_x_posHi'][idx]*256 + repvars['player_x_posLo'][idx])
+        
+        # Look for clips
+        for current_scene in scenes_info_dict.keys():
+            print(current_scene)
+            scenes_info_found = []
+            print(f'Scene {current_scene} : start = {scenes_info_dict[current_scene]["start"]}, end = {scenes_info_dict[current_scene]["end"]}')
+            start_found = False
+            start_range = [x for x in range(scenes_info_dict[current_scene]['start']-2, scenes_info_dict[current_scene]['start']+2)]
+            ## TODO manage issue with bonus scenes
+            for frame_idx in range(1, n_frames_total):
+                if not start_found:
+                    # Look for start
+                    if repvars['player_x_pos'][frame_idx] in start_range:#>= scenes_info_dict[current_scene]['start'] and repvars['player_x_pos'][frame_idx-1] < scenes_info_dict[current_scene]['start'] and repvars['player_x_pos'][frame_idx] < scenes_info_dict[current_scene]['end']:
+                        start_idx = frame_idx
+                        start_found = True
+                else:
+                    # Look for end
+                    if repvars['player_x_pos'][frame_idx] >= scenes_info_dict[current_scene]['end'] and repvars['player_x_pos'][frame_idx-1] < scenes_info_dict[current_scene]['end']:
+                        end_idx = frame_idx
+                        start_found = False
+                        scenes_info_found.append([start_idx, end_idx])
+                    elif repvars['player_x_pos'][frame_idx] >= scenes_info_dict[current_scene]['start'] and repvars['player_x_pos'][frame_idx-1] < scenes_info_dict[current_scene]['start']:
+                        start_idx = frame_idx
+            for pat_idx, pattern in enumerate(scenes_info_found):
+                onset.append(pattern[0]/FS)
+                duration.append((pattern[1]-pattern[0])/FS)
+                trial_type.append(f'scene-{current_scene}')
+                level.append(repvars['level'])
+                frame_start.append(pattern[0])
+                frame_stop.append(pattern[1])
+
+    events_df = pd.DataFrame(data={'onset':onset,
+                            'duration':duration,
+                            'trial_type':trial_type,
+                            'level':level,
+                            'frame_start':frame_start,
+                            'frame_stop':frame_stop})
     return events_df
 
 def main(args):
+    FS = 60
+
     # Get datapath
     DATA_PATH = args.datapath
     if DATA_PATH == ".":
@@ -300,6 +407,24 @@ def main(args):
     else:
         stimuli_path = op.join(args.stimuli)
     retro.data.Integrations.add_custom_path(stimuli_path)
+
+    # Load scenes info
+    SCENES_FILE = args.scenesfile
+    if SCENES_FILE is None:
+        SCENES_FILE = op.join(DATA_PATH, "scenes.json")
+    SCENES_FILE = op.join('/home/hyruuk/GitHub/neuromod/videogames_curiosity', "clip_extractor","scenes_mastersheet.csv")
+
+    
+    scenes_info = pd.read_csv(SCENES_FILE)
+
+    #scenes_info = correct_xscroll(scenes_info)
+    scenes_info_dict = {}
+    for idx, row in scenes_info.iterrows():
+        scene_id = f'w{row["World"]}l{row["Level"]}s{row["Scene"]}'
+        scenes_info_dict[scene_id] = {}
+        scenes_info_dict[scene_id]['start'] = row['Entry point']
+        scenes_info_dict[scene_id]['end'] = row['Exit point']
+
     
     # Walk through all folders looking for .bk2 files
     for root, folder, files in sorted(os.walk(DATA_PATH)):
@@ -323,14 +448,21 @@ def main(args):
                                     pkl_sidecar_fname = bk2_file.replace(".bk2", ".pkl")
                                     with open(pkl_sidecar_fname, 'rb') as f:
                                         repvars = pickle.load(f)
+                                    # Add info to repetition event
                                     events_dataframe.loc[events_dataframe['stim_file']==bk2_file, 'level'] = repvars['level'] # replace level value in the dataframe by the one in the repvars dict
+                                    events_dataframe.loc[events_dataframe['stim_file']==bk2_file, 'frame_start'] = int(0)
+                                    events_dataframe.loc[events_dataframe['stim_file']==bk2_file, 'frame_stop'] = int(len(repvars['score']))
+                                    events_dataframe.loc[events_dataframe['stim_file']==bk2_file, 'duration'] = int(len(repvars['score']))/FS
+                                    # rename index column to rep_index
+                                    events_dataframe.rename(columns={'index':'rep_index'}, inplace=True)
+
                                     runvars.append(repvars)
                             else:
                                 print("Missing file, skipping")
                                 runvars.append({})
-                        events_df = create_runevents(runvars, events_dataframe)
+                        events_df = create_runevents(runvars, scenes_info_dict, events_dataframe, FS=FS)
                         events_df.to_csv(events_annotated_fname, sep='\t', index=False)
-
+                        
 if __name__ == "__main__":
     args = parser.parse_args()
     main(args)
